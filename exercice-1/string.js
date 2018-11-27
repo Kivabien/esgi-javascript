81655fd55bc507cec4ec462fb01ba60d60560669
MALLET Thomas <thomas.mallet45@gmail.com>
2018-11-27 10:49:32 +0100

    first commit

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -2,7 +2,13 @@ var str = "hello word";
 
 function ucfirst(str) 
 {
+	if( typeof str == "string") {
     return str.charAt(0).toUpperCase() + str.slice(1);
+	}
+	else{
+	var hello = "";
+	return hello;
+	}
 }
 
 console.log(ucfirst(str));
