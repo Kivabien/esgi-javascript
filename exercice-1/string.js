d02ce037c032046695cc026b40c55b017c7dd99c
MALLET Thomas <thomas.mallet45@gmail.com>
2018-12-19 13:54:50 +0100

    commit

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -1,4 +1,8 @@
+//Declaration de la variable :
+
 var str = "hello word";
+
+// ucfirst
 
 function ucfirst(str) 
 {
@@ -6,52 +10,198 @@ function ucfirst(str)
 		return str.charAt(0).toUpperCase() + str.slice(1);
 	}
 	else{
-		var hello = "";
-		return hello;
+		return "";
 	}
 }
 
-var str = "hello word";
+// capitalize
 
-function capitalize(str) 
-{
-    str = str.split(" ");
-
-    for (var i = 0, x = str.length; i < x; i++) {
-        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+function capitalize(str) {
+	if (typeof str === "string") {
+		return str.toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(' ');
+    } else {
+    	return "";
     }
-
-    return str.join(" ");
 }
 
-var str = "hello word";
+// camelCase
 
 function camelCase(str) {
-      return str.replace(/\W+(.)/g, function(match, chr)
-        {
-            return chr.toUpperCase();
-         });
-     }
-
-var message = "Anaconda";
-
-function leet(chr) {
-	var chrs = {'a':'4','e':'3','i':'1','o':'O','u':'(_)','y':'7'};
-	return chrs[chr.toLowerCase()] || chr;
+	if (typeof str === "string") {
+		return str.toLowerCase()
+    .split(/_| /)
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join('');
+    } else {
+    	return "";
+    }
 }
 
-var str  = "hello world";
+// snake_case
 
-function verlan(str){
-	if(str === ""){
-		return str ;
-	}else{
-		return verlan(str.substr(1)) + str[0];
+function snake_case(str) {
+    if (typeof str == 'string') {
+        return str.replace(/ /g, '_').toLowerCase();
+	}
+    else{
+		return "";
 	}
 }
+
+// leet
+
+function leet(str){
+	if( typeof str === 'string' ){
+		var mot = "";
+		for (var i = 0; i < str.length; i++) {
+			var lettre = str.charAt(i);
+			switch(lettre){
+				
+				case "a":
+					lettre = "4";
+					break;
+					
+				case "A":
+					lettre = "4";
+					break;
+					
+				case "e":
+					lettre = "3";
+					break;
+					
+				case "E":
+					lettre = "3";
+					break;
+					
+				case "i":
+					lettre = "1";
+					break;
+					
+				case "I":
+					lettre = "1";
+					break;
+					
+				case "o":
+					lettre = "0";
+					break;
+					
+				case "O":
+					lettre = "0";
+					break;
+					
+				case "u":
+					lettre = "(_)";
+					break;
+					
+				case "U":
+					lettre = "(_)";
+					break;
+					
+				case "y":
+					lettre = "7";
+					break;
+					
+				case "Y":
+					lettre = "7";
+					break;
+			}
+			mot = mot + lettre;
+		}
+		return mot;
+	}else{
+		return '';
+	}
+}
+
+// prop_access
+
+function prop_access(object, path) {
+    if (typeof path != "string"){
+        return object;
+    }
+
+    if(typeof object != 'object' || object == null) {
+        console.log(path + ' not exist');
+        return;
+    }
+    if (path === null || path === '') {
+        return object;
+    }
+
+    const props = path.split('.');
+    let property = object;
+    props.forEach(function (prop) {
+        if(!property.hasOwnProperty(prop)) {
+            console.log(path + ' not exist');
+            return;
+        }
+        property = property[prop];
+    });
+    return property;
+}
+
+var prairie =
+{
+  animal : {
+    type : {
+      name : "chien"
+    }
+  }
+}
+
+// verlan
+
+function verlan(str) {
+	if (typeof str === "string") {
+		return str.split("").reverse().join("").split(" ").reverse().join(" ");
+	} else {
+		return "";
+	}
+}
+
+
+// yoda 
+
+function yoda(str) {
+	if (typeof str === "string") {
+		return str.split(" ").reverse().join(" ");
+	} else {
+		return "";
+	}
+}
+
+// vig
+
+function vig(mot, clef) {
+  if (typeof mot != "string" || typeof clef != "string"){
+    return "";
+  }
+  motCrypte = "";
+  cptSpace = 0;
+  for(var i = 1;i <= mot.length; i++) {
+    if(mot.charAt(i-1) != " ") {
+      codeLettre = mot.toUpperCase().charCodeAt(i-1)-65;
+      decalage = clef.toUpperCase().charCodeAt((i-1-cptSpace)%clef.length)-65;
+      lettre = String.fromCharCode(65+(codeLettre+decalage)%26);
+      motCrypte = motCrypte.concat('', lettre);
+    } else {
+      cptSpace++
+      motCrypte = motCrypte.concat('', ' ');
+    }
+  }
+  return motCrypte.toLowerCase();
+}
+
+// utilisation des fonctions 
 
 console.log(ucfirst(str));
 console.log(capitalize(str));
 console.log(camelCase(str));
-console.log( message.replace(/[aeltso]/g, leet) );
+console.log(snake_case(str));
+console.log(leet(str));
+console.log(prop_access(prairie,"animal.type.name"));
 console.log(verlan(str));
+console.log(yoda(str));
+console.log(vig("wikipedia","crypto"));
